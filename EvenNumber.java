c6b7ea054294862256723c0e4bc6dbe696d1768f
vivek kumar <vivekshrivastavasiwan@gmail.com>
2024-06-12 03:04:50 -0700

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,13 @@
+package loops;
+public class EvenNumber {
+    public static void main(String[] args){
+        // 100 iteration (continue)
+        for(int i = 1; i<=100; i++){
+            if(i%2 == 1) continue;
+            System.out.println(i+"");   
+            
+        }
+    }
+
+    
+}
